6933e51afc3da754a9869b2997a84e39d32ad9f9
Paweł Walczykiewicz <p.walczykiewicz@dotandspot.pl>
2018-03-22 20:03:27 +0100

    Fancy wyswietlanie itemow

--- resources/views/front/offer_list.blade.php
+++ resources/views/front/offer_list.blade.php
@@ -5,13 +5,27 @@
 
     @foreach($items as $i)
         <div class="col-md-4 item-div">
-            <img class="col-md-12" src="{{ asset("img/$i->img_front")}}" alt="front" style="margin-bottom: 5px;">
+            <div class="col-md-12 text-center item-name" style="color: #CEBCED; font-size: 18px; height: 210px;">
+                <div class="flip-container" ontouchstart="this.classList.toggle('hover');" style="height: 100%;">
+                    <div class="flipper" style="height: 100%;">
+                        <div class="front" style="height: 100%; width: 100%;">
+                            <img class="col-md-12" src="{{ asset("img/$i->img_front")}}" alt="front" style="margin-bottom: 5px;">
+                        </div>
+                        <div class="back" style="height: 100%; width: 100%;">
+                            <img class="col-md-12" src="{{ asset("img/$i->img_back")}}" alt="front" style="margin-bottom: 5px;">
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="col-md-12 text-center item-name" style="color: #CEBCED; font-size: 18px;">{{$i->name}}</div>
         </div>
     @endforeach
 
 </div>
-<?php echo $items->render(); ?>
+<div class="col-xs-12 text-center">
+    <?php echo $items->render(); ?>
+</div>
+
 
 
 @stop
