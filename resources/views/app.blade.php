6a4c85f8aeb1647fa78558bb53cdd22838b1c267
almono <p.walczykiewicz@dotandspot.pl>
2019-02-27 22:06:21 +0000

    Removed css file link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-    <link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}"/>
-
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ URL::asset('css/all.css') }}">
     <script type="text/javascript" src="{{URL::asset('js/all.js')}}"></script>
